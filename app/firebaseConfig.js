c8936c5401b6afd38b34d60a52258a2466d12ed7
alexandrebhz2026 <alexandre@spotpass.com.br>
2026-08-24 10:40:55 -0300

    RAIO-X: connect Firebase project for real authentication

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -4,12 +4,12 @@ import {getAuth, getReactNativePersistence, initializeAuth} from 'firebase/auth'
 import {getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY || '',
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN || '',
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID || '',
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET || '',
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '',
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID || '',
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY || 'AIzaSyAmnbDT48iQW8SpxUZyTQh__HwM0yWgOwY',
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN || 'raioxeleicoes2026.firebaseapp.com',
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID || 'raioxeleicoes2026',
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET || 'raioxeleicoes2026.firebasestorage.app',
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '982564347981',
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID || '1:982564347981:android:b4cdbb08002ed98226fc04',
 };
 
 export const firebaseReady = Boolean(firebaseConfig.apiKey && firebaseConfig.projectId && firebaseConfig.appId);
